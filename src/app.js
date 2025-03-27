7bed10e5a94be79958aa78da79fedc7df854de2f
JJAYTECH <jayrcanete123@gmail.com>
2025-03-27 23:18:20 +0800

    group-C

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 import express from 'express'
-import {router }from './routes/examGroup.js'
+import bodyParser from 'body-parser' 
+import router from './routes/routesGroup.js'
 
 const app = express()
 
-
-app.use(bodyParser.json()); 
-let exams = [
-  { id: 1, name: 'Math Exam', date: '2025-03-25' },
-  { id: 2, name: 'Science Exam', date: '2025-03-26' }
-];
+app.use(bodyParser.json())
 
 app.use('/', router)
-//COMMENT FOR CONFLICT
+
 app.listen(3000, () => {
-  console.log('Server running on port : 3000')
+  console.log('Server running on port: 3000')
 })
